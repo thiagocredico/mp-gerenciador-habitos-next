43ca3214e9d97832fffb5f09494ceebf081adcb6
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-08-26 16:24:20 -0300

    feat: delete button

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { deleteHabit } from "@/app/actions";
+
 import Image from "next/image";
 
 function DeleteButton({ habit }: { habit: string }) {
   return (
-    <button>
+    <button onClick={() => deleteHabit(habit)}>
       <Image
         src="/images/trash.svg"
         width={20}
