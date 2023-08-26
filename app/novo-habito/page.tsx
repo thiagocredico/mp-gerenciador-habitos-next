49a1059d516f8613dc3c25f028b433bcaca92fd6
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-08-26 18:40:35 -0300

    fix: correct logic for submit and cancel button

--- app/novo-habito/page.tsx
+++ app/novo-habito/page.tsx
@@ -1,42 +1,48 @@
-import { kv } from "@vercel/kv";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { kv } from '@vercel/kv';
+import { revalidatePath } from 'next/cache';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 function NewHabit() {
   async function newHabit(formData: FormData) {
-    "use server";
+    'use server';
 
-    const habit = formData.get("habit");
-    await kv.hset("habits", { [habit as string]: {} });
+    const habit = formData.get('habit');
+    if (habit) {
+      await kv.hset('habits', { [habit as string]: {} });
+    }
 
-    revalidatePath("/");
-    redirect("/");
+    revalidatePath('/');
+    redirect('/');
   }
 
   return (
-    <main className="container relative flex flex-col gap-8 px-12 pt-16">
-      <h1 className="text-4xl font-light text-center text-white font-display">
+    <main className='container relative flex flex-col gap-8 px-12 pt-16'>
+      <h1 className='text-4xl font-light text-center text-white font-display'>
         novo hábito
       </h1>
 
-      <form action={newHabit} className="flex flex-col gap-4 mt-4">
+      <form action={newHabit} className='flex flex-col gap-4 mt-4'>
         <input
-          type="text"
-          name="habit"
-          id="habit"
-          className="p-2 font-sans text-xl text-white rounded-md bg-neutral-800"
+          type='text'
+          name='habit'
+          id='habit'
+          className='p-2 font-sans text-xl text-white rounded-md bg-neutral-800'
         />
 
         <button
-          type="submit"
-          className="bg-[#45EDAD] font-display text-neutral-900 font-regular text-2xl p-2 rounded-md mt-8"
+          type='submit'
+          className='bg-[#45EDAD] font-display text-neutral-900 font-regular text-2xl p-2 rounded-md mt-8'
         >
           cadastrar
         </button>
 
-        <button className="bg-neutral-800 text-[#F85858] font-display font-regular text-2xl p-2 rounded-md">
-          cancelar
-        </button>
+        <Link
+          href='/'
+          className='text-center bg-neutral-800 text-[#F85858] font-display font-regular text-2xl p-2 rounded-md'
+        >
+          <button type='button'>cancelar</button>
+        </Link>
       </form>
     </main>
   );
